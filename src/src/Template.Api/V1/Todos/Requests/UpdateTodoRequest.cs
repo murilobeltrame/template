d658b04d84b631fa8ed9b5c6322fc94d8ea5fe7c
murilobeltrame <murilobeltrame@gmail.com>
2024-06-02 01:50:21 -0300

    documenting endpoint

---
+++ src/src/Template.Api/V1/Todos/Requests/UpdateTodoRequest.cs
@@ -0,0 +1,12 @@
+﻿using Template.Application.TodoUsecases.Commands;
+
+namespace Template.Api.V1.Todos.Requests;
+
+public class UpdateTodoRequest(string title, string description, DateTime? dueDate)
+{
+    public string Title { get; } = title;
+    public string Description { get; } = description;
+    public DateTime? DueDate { get; } = dueDate;
+
+    internal UpdateTodoCommand ToCommandWithId(int id) => new(id, Title, Description, DueDate);
+}
--- src/src/Template.Api/V1/Todos/TodosController.cs
+++ src/src/Template.Api/V1/Todos/TodosController.cs
@@ -1,9 +1,13 @@
+using System.Net;
+
 using MediatR;
 
 using Microsoft.AspNetCore.Mvc;
 
+using Template.Api.V1.Todos.Requests;
 using Template.Application.TodoUsecases.Commands;
 using Template.Application.TodoUsecases.Queries;
+using Template.Domain.Todos;
 
 namespace Template.Api.V1.Todos;
 
@@ -13,29 +17,54 @@ public class TodosController(IMediator mediator) : ControllerBase {
     private readonly IMediator _mediator = mediator;
 
     [HttpGet]
-    public async Task<IActionResult> Fetch(FetchTodosQuery query) {
+    [ProducesResponseType<IEnumerable<Todo>>((int)HttpStatusCode.OK)]
+    [ProducesResponseType<ProblemDetails>((int)HttpStatusCode.BadRequest)]
+    public async Task<IActionResult> Fetch([FromQuery] FetchTodosQuery query) {
         return Ok(await _mediator.Send(query));
     }
 
     [HttpGet("{id:int}")]
+    [ProducesResponseType<Todo>((int)HttpStatusCode.OK)]
+    [ProducesResponseType<ProblemDetails>((int)HttpStatusCode.BadRequest)]
+    [ProducesResponseType<ProblemDetails>((int)HttpStatusCode.NotFound)]
     public async Task<IActionResult> Get(int id) {
         var result = await _mediator.Send(new GetTodoByIdQuery(id));
         return result != null ? Ok() : NotFound();
     }
 
     [HttpPost]
+    [ProducesResponseType<Todo>((int)HttpStatusCode.Created)]
+    [ProducesResponseType<ProblemDetails>((int)HttpStatusCode.BadRequest)]
+    [ProducesResponseType<ProblemDetails>((int)HttpStatusCode.Conflict)]
+    [ProducesResponseType<ProblemDetails>((int)HttpStatusCode.PreconditionRequired)]
+    [ProducesResponseType<ProblemDetails>((int)HttpStatusCode.PreconditionFailed)]
+    [ProducesResponseType<ProblemDetails>((int)HttpStatusCode.UnprocessableEntity)]
     public async Task<IActionResult> Create([FromBody] CreateTodoCommand command) {
         var result = await _mediator.Send(command);
         return CreatedAtAction(nameof(Get), new {id = 1}, result);
     }
 
     [HttpPut("{id:int}")]
-    public async Task<IActionResult> Update(int id, [FromBody] UpdateTodoCommand command) {
-        await _mediator.Send(command);
+    [ProducesResponseType((int)HttpStatusCode.NoContent)]
+    [ProducesResponseType<ProblemDetails>((int)HttpStatusCode.BadRequest)]
+    [ProducesResponseType<ProblemDetails>((int)HttpStatusCode.NotFound)]
+    [ProducesResponseType<ProblemDetails>((int)HttpStatusCode.Conflict)]
+    [ProducesResponseType<ProblemDetails>((int)HttpStatusCode.PreconditionRequired)]
+    [ProducesResponseType<ProblemDetails>((int)HttpStatusCode.PreconditionFailed)]
+    [ProducesResponseType<ProblemDetails>((int)HttpStatusCode.UnprocessableEntity)]
+    public async Task<IActionResult> Update(int id, [FromBody] UpdateTodoRequest request) {
+        await _mediator.Send(request.ToCommandWithId(id));
         return NoContent();
     }
 
     [HttpDelete("{id:int}")]
+    [ProducesResponseType((int)HttpStatusCode.NoContent)]
+    [ProducesResponseType<ProblemDetails>((int)HttpStatusCode.BadRequest)]
+    [ProducesResponseType<ProblemDetails>((int)HttpStatusCode.NotFound)]
+    [ProducesResponseType<ProblemDetails>((int)HttpStatusCode.Conflict)]
+    [ProducesResponseType<ProblemDetails>((int)HttpStatusCode.PreconditionRequired)]
+    [ProducesResponseType<ProblemDetails>((int)HttpStatusCode.PreconditionFailed)]
+    [ProducesResponseType<ProblemDetails>((int)HttpStatusCode.UnprocessableEntity)]
     public async Task<IActionResult> Delete(int id) {
         await _mediator.Send(new DeleteTodoCommand(id));
         return NoContent();
